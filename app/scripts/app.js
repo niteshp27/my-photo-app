95fa65df61075728da3d80f6ef525ea1e2d6722e
Anton Dobrev <anton.dobrev@telerik.com>
2014-12-01 08:42:16 +0000

    Initial commit.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,59 @@
-(function() {
+(function () {
     document.addEventListener("deviceready", function () {
-        var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        
+        var everliveApiKey = 'YOUR_API_KEY';
+        var everliveScheme = 'https';
+
+        var everlive = new Everlive({
+            apiKey: everliveApiKey,
+            scheme: everliveScheme
+        });
+
+        window.listview = kendo.observable({
+            addImage: function () {
+                var success = function (data) {
+                    everlive.Files.create({
+                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                        ContentType: "image/jpeg",
+                        base64: data,
+                    }).then(function (data) {
+                        alert(JSON.stringify(data.result));
+                        loadPhotos();
+                    });
+                };
+                var error = function () {
+                    navigator.notification.alert("Unfortunately we could not add the image");
+                };
+                
+                var config = {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    targetHeight: 400,
+                    targetWidth: 400,
+                    encodingType: Camera.EncodingType.PNG
+                };
+                navigator.camera.getPicture(success, error, config);
+            }
+        });
+
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "flat"
+        });
+
+        function loadPhotos() {
+            everlive.Files.get().then(function (data) {
+                var files = [];
+                data.result.forEach(function (image) {
+                    files.push(image.Uri);
+                });
+                $("#images").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #'>"
+                });
+            });
+        };
+
+        loadPhotos();
+
+
     });
 }());
